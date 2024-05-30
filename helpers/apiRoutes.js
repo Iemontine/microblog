989c6b7b684729840a4cbc2bbcbe4bbe4689ea9c
lemontine <computereality@gmail.com>
2024-05-29 23:23:43 -0700

    fixed slight misnamings with files and added error catching to oauth

--- helpers/apiRoutes.js
+++ helpers/apiRoutes.js
@@ -33,35 +33,39 @@ router.get('/auth/google/', (req, res) => {
 
 // Handle Google OAuth response
 router.get('/auth/google/callback', async (req, res) => {
-	const { code } = req.query;
-	const { tokens } = await client.getToken(code);
-	client.setCredentials(tokens);
-	const oauth2 = google.oauth2({
-		auth: client,
-		version: 'v2',
-	});
-	const userinfo = await oauth2.userinfo.get();
+	try {
+		const { code } = req.query;
+		const { tokens } = await client.getToken(code);
+		client.setCredentials(tokens);
+		const oauth2 = google.oauth2({
+			auth: client,
+			version: 'v2',
+		});
+		const userinfo = await oauth2.userinfo.get();
 
-	// Check if user is registering or logging in
-	if (req.session.registering) {
-		req.session.registering = false;
-		try {
-			// Register user, if successful generate avatar
-			if (helper.registerUser(req, res, userinfo)) {
-				await helper.handleAvatar(req, res);
+		// Check if user is registering or logging in
+		if (req.session.registering) {
+			req.session.registering = false;
+			try {
+				// Register user, if successful generate avatar
+				if (helper.registerUser(req, res, userinfo)) {
+					await helper.handleAvatar(req, res);
+				}
+			} catch (error) {
+				console.error(error);
 			}
-		} catch (error) {
-			console.error(error);
 		}
-	}
-	else if (req.session.loggingIn) {
-		req.session.loggingIn = false;
-		try {
-			// Login user
-			helper.loginUser(req, res, userinfo)
-		} catch (error) {
-			console.error(error);
+		else if (req.session.loggingIn) {
+			req.session.loggingIn = false;
+			try {
+				// Login user
+				helper.loginUser(req, res, userinfo)
+			} catch (error) {
+				console.error(error);
+			}
 		}
+	} catch (error) {
+		console.error(error);
 	}
 });
 
--- helpers/webRoutes.js
+++ helpers/webRoutes.js
@@ -6,7 +6,7 @@ const multer = require('multer');
 const path = require('path');
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, '/public/uploads/'); // Destination folder for uploaded files
+		cb(null, './public/uploads/'); // Destination folder for uploaded files
 	},
 	filename: function (req, file, cb) {
 		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
